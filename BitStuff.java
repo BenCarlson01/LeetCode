18010d71645597b3f8f5f495b77b6364b0f6b334
bcarlson01 <bcarlson@berkeley.edu>
2018-09-23 11:23:52 -0700

    Doing some more bit stuff

--- BitStuff.java
+++ BitStuff.java
@@ -8,4 +8,41 @@ public class BitStuff {
         }
         return count;
     }
+
+    /** I did this by myself! */
+    public int reverseBits1(int n) {
+        int pos = 0, ans = 0;
+        while (pos < 16) {
+            ans |= (n & (1 << pos)) << (31 - 2 * pos);
+            ans |= (n & (1 << (31 - pos))) >>> (31 - 2 * pos);
+            pos += 1;
+        }
+        return ans;
+    }
+
+    public int reverseBits2(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i += 1) {
+            ans = ans << 1;
+            ans |= n & 1;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; i += 1) {
+            ans[i] = ans[i >> 1] + i % 2;
+        }
+        return ans;
+    }
+
+    public int missingNumber(int[] nums) {
+        int bit = nums.length;
+        for (int i = 0; i < nums.length; i += 1) {
+            bit ^= i ^ nums[i];
+        }
+        return bit;
+    }
 }
